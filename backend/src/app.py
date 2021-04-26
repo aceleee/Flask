0d8bbead50c9aa1d2607fa889d6b2cba1504c856
lixiang <956313445@qq.com>
2021-04-26 22:24:33 +0800

    环境搭建

---
+++ backend/src/app.py
@@ -0,0 +1,12 @@
+#codeing=utf-8
+from common import *
+app.secret_key = '123456'
+
+queue = Query()
+db = queue.conn()
+@app.route('/')
+def first():
+    return app.send_static_file("index.html")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
